3a19cbd763e90dca468df1986f5b3bdb1995cb1a
BeEz <98581184+Beez1@users.noreply.github.com>
2024-08-21 23:07:37 +0100

    home page and nav implementation

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
@@ -8,6 +10,20 @@ import { RouterModule } from '@angular/router';
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+[x: string]: any;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      document.addEventListener('DOMContentLoaded', () => {
+        const navToggle = document.querySelector('.nav-toggle');
+        const navLinks = document.querySelector('.nav-links');
+
+        navToggle?.addEventListener('click', () => {
+          navLinks?.classList.toggle('active');
+        });
+      });
+    }
+  }
 }
